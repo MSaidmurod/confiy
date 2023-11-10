acd6414cf254b861f06976818261feeaa1e75297
MSaidmurod <129066659+MSaidmurod@users.noreply.github.com>
2023-11-10 18:58:54 +0500

    Add files via upload

---
+++ mebel/src/pages/Order.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Order() {
+  return (
+    <div className="max-6xl mx-auto mt-16">
+      <div className=" text-center mx-5 max-lg:mx-5 mt-5">
+        <button className="text-3xl pb-3  btn btn-primary font-bold">
+          Haridingiz uchun raxmat 👌
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Order;
